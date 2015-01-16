ec139aacb0c4012aea5cf21faa235ec8f6763c28
Emil Melnikov <emilmelnikov@gmail.com>
2015-01-16 19:01:03 +0300

    Change DB to PostgreSQL

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   root 'users#edit'
   resource :users, only: [:edit, :update], as: :user
 
-  devise_for :users
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
+  devise_for :users
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
